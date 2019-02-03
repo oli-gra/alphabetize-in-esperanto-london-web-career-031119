118fab3115f99b39003634ccace09a7fef832ba0
Oliver <oliver.grant@gmail.com>
2019-02-03 11:47:13 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,5 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by do |phrase| 
-    phrase.split("").collect {|char| ESPERANTO_ALPHABET.index(char)}
-  end	
+  arr.sort_by {|phrase| phrase.split("").collect {|char| ESPERANTO_ALPHABET.index(char)}}
 end
